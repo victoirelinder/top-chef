e676f3ba1ba9ce1bd9554c43ad8278d9e30f597c
victoirelinder <victoirelinder@hotmail.com>
2018-02-11 12:20:17 +0100

    list of restaurant scrap

---
+++ src/server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+url ='https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin';
+  // The first parameter is our URL
+    // The callback function takes 3 parameters, an error, response status code and the html
+ request(url, function(error, response, html){
+ 	  // First we'll check to make sure no errors occurred when making the request
+ 	 if(!error){
+ 	 	var $ = cheerio.load(html);
+ 	 	    // Finally, we'll define the variables we're going to capture
+
+            var title;
+            var json = { title : ""};
+             $('.header').filter(function(){
+
+           // Let's store the data we filter into a variable so we can easily see what's going on.
+
+                var data = $(this);
+
+           // Utilizing jQuery we can easily navigate and get the text by writing the following code:
+
+                title = data.text().trim();
+
+           // Once we have our title, we'll store it to the our json object.
+
+                json.nomdurestau = nomdurestau;
+        }
+        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+
+   			 console.log('Done ! - Check your project directory for the output.json file');
+
+		})
+
+		// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+		res.send('Check your console!')
+    })
+}
